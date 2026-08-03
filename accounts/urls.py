8b0e1d65f6cffd88a6cc000b2745b572bd146dec
mvulch <161260528+mvulch@users.noreply.github.com>
2026-08-03 16:46:23 +0300

    Added logic for password change.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from .views import register_view, login_view, profile_view, CustomPasswordChangeView, CustomLogoutView
 from django.contrib.auth import views as auth_views
 
@@ -9,5 +9,21 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('password-change/', CustomPasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html',
+        email_template_name='accounts/emails/password_reset_email.txt',
+        success_url=reverse_lazy('accounts:password_reset_done')
+    ),name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+
+    path('password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url=reverse_lazy('accounts:password_reset_complete')
+    ), name='password_reset_confirm'),
+
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
